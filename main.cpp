6b9ff252bb292cf8d688e91082c667bf52f3cc86
Soumik Seth <soumikseth8@gmail.com>
2024-12-10 17:51:54 +0530

    Render all objects on the screen

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include <SFML/Graphics.hpp>
 
 int main() {
@@ -11,13 +10,62 @@ int main() {
     spriteBackground.setTexture(textureBackground);
     spriteBackground.setPosition(0, 0);
 
+    sf::Texture textureTree;
+    textureTree.loadFromFile("../graphics/tree.png");
+    sf::Sprite spriteTree;
+    spriteTree.setTexture(textureTree);
+    spriteTree.setPosition(810, 0);
+
+    sf::Texture textureBee;
+    textureBee.loadFromFile("../graphics/bee.png");
+    sf::Sprite spriteBee;
+    spriteBee.setTexture(textureBee);
+    spriteBee.setPosition(0, 800);
+
+    bool beeActive = false;
+
+    float beeSpeed = 0.0f;
+
+
+    sf::Texture textureCloud;
+    textureCloud.loadFromFile("../graphics/cloud.png");
+
+    sf::Sprite spriteCloud1, spriteCloud2, spriteCloud3;
+    spriteCloud1.setTexture(textureCloud);
+    spriteCloud2.setTexture(textureCloud);
+    spriteCloud3.setTexture(textureCloud);
+
+    spriteCloud1.setPosition(0, 0);
+    spriteCloud2.setPosition(0, 250);
+    spriteCloud3.setPosition(0, 500);
+
+    bool cloud1Active = false;
+    bool cloud2Active = false;
+    bool cloud3Active = false;
+
+    float cloud1Speed = 0.0f;
+    float cloud2Speed = 0.0f;
+    float cloud3Speed = 0.0f;
+
     while (window.isOpen()) {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
             window.close();
         }
         window.clear();
+
         window.draw(spriteBackground);
+
+        window.draw(spriteCloud1);
+        window.draw(spriteCloud2);
+        window.draw(spriteCloud3);
+        
+
+        window.draw(spriteTree);
+
+        window.draw(spriteBee);
+
         window.display();
     }
+
     return 0;
 }
